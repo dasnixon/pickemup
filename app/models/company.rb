6aedef12a421eab4753d6cfcb329fbdfc71ac02b
tomdepplito <tomdepplito@gmail.com>
2013-07-07 14:07:45 -0700

    Added ability to edit company info

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,16 +1,21 @@
 class Company < ActiveRecord::Base
   attr_accessor :password
-  before_save :encrypt_password
+  before_save :encrypt_password, :clean_url
 
   validates_confirmation_of :password, :message => "Password/Password Confirmation is invalid"
   validates_presence_of :password, :on => :create
   validates_presence_of :email
   validates_uniqueness_of :email
-  validate :password_strength
-  validates_format_of :email, :with => /[\w|\d]{1,}@[\w|\d]*[.][\w|\d]*/, :on => :create, :message => "Email is invalid."
+  validate :password_strength, :on => :create
+  validates_format_of :email, :with => /[\w|\d]{1,}@[\w|\d]*[.][\w|\d]*/, :message => "Email is invalid."
 
   def password_strength
     errors.add(:password_length, "Password must be at least 8 characters") unless password.length >= 8
+  end
+
+  def clean_url
+    new_url = website.gsub(/(https{0,1}:\/\/)|(www)./, "").prepend("http://")
+    self.website = new_url
   end
 
   def self.authenticate(email, password)
