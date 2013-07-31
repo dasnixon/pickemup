2026e53437e0d8ece48a8533270aa7c4f2918b80
tomdepplito <tomdepplito@gmail.com>
2013-07-30 20:56:29 -0700

    Added SendGrid notification for new companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -7,8 +7,9 @@ class CompaniesController < ApplicationController
     unless params[:company][:email].blank? || params[:company][:password].blank? || params[:company][:password_confirmation].blank?
       @company.founded = Time.now
       if @company.save
+        Notifier.new_company_confirmation(@company).deliver
         session[:company_id] = @company.id
-        redirect_to new_company_subscription_path(:company_id => @company.id), notice: "You've just created a new company!"
+        redirect_to new_company_subscription_path(:company_id => @company.id), notice: "You've just created a new company.  You will receive an email to verify your account in a moment."
       else
         @company.errors.messages.each { |error, message| flash[:error] = message.join(', ') }
         redirect_to :back
@@ -27,13 +28,21 @@ class CompaniesController < ApplicationController
 
   def update
     @company.update(company_params)
-    #TODO add JS validation of password/password_confirmation fields
     @company.password = params[:company][:password] if params[:company][:password] == params[:company][:password_confirmation]
     if @company.save
       redirect_to root_path, notice: "Info updated!"
     else
       flash[:error] = "Something went wrong"
       render :edit
+    end
+  end
+
+  def validate_company
+    @company = Company.find_by_email(params[:email])
+    if @company
+      @company.verified = true
+      @company.save
+      redirect_to root_path, notice: "You can now start searching for developers!"
     end
   end
 
