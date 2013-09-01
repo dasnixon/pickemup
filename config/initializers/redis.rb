b852a048afe96e7cb52596569c5c1cf8045ec2ae
Chris Nixon <chris.d.nixon@gmail.com>
2013-08-31 18:01:57 -0700

    Added redis fix

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,3 @@
-$redis = Redis.new
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/")
+$redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
 Soulmate.redis = $redis
